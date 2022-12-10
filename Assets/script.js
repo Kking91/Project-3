393ab85dc464727649240dac7df7d66d2bd54de2
Kiefer King <kiefydking@yahoo.com>
2022-12-10 04:43:07 -0600

    cleaning up the folder and adding screenshots

--- Assets/script.js
+++ Assets/script.js
@@ -14,6 +14,7 @@ var generateBtn = document.querySelector("#generate");
   var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var capitalArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   
+  //  Decides which arrays will be used to generate the password
   var lowerCase = confirm ("Would you like your password to contain lowercase letters in it?");
   var upperCase = confirm ("Would you like your password to contain uppercase letters in it?");
   var numbers = confirm ("Would you like your password to contain numbers in it?");
@@ -33,6 +34,7 @@ var generateBtn = document.querySelector("#generate");
   if (characters) {
     selectedArray = selectedArray.concat(specialArray);
   }
+  //  Tells the user they have to choose one of the options to get a password
   if (!upperCase && !lowerCase && !numbers && !characters) {
     window.alert("You must choose at least one of the options.");
     return "Try again";
@@ -45,7 +47,7 @@ var generateBtn = document.querySelector("#generate");
 
   return passwordArray;
 }
-//  Setting the lenghth of the generated password
+//  Setting the length of the generated password
 function getPasswordLength() {
   var clientsNumber = (prompt("Please enter a number between 8 and 128"));
   if (clientsNumber < 8 || clientsNumber > 128) {
